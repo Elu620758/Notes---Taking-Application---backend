10c592b0f86bcbfc13edd1bace41ce567f448be4
Elu620758 <elumalai620758@gmail.com>
2023-11-21 21:19:47 +0530

    first commit

---
+++ Db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongodb = process.env.MONGODB_URL;
+
+const connection = mongoose.connect(mongodb, {})
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error.message);
+    });
+
+module.exports = {
+    connection,
+};
